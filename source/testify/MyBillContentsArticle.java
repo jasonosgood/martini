4803595fbc943fcc11a141d53dee0a1f824b3eea
jasonosgood <jason@jasonosgood.com>
2013-11-29 16:23:55 -0800

    location of text files was hardcoded

--- source/testify/MyBillContentsArticle.java
+++ source/testify/MyBillContentsArticle.java
@@ -1,6 +1,7 @@
 package testify;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import martini.HTMLBuilder;
@@ -12,21 +13,42 @@ public class
 extends 
 	BillContentsArticle
 {
+	private String _root = null;
+	
+	public void setRoot( String root ) 
+		throws FileNotFoundException
+	{
+		if( root == null )
+		{
+			throw new NullPointerException( "root" );
+		}
+		File temp = new File( root );
+		if( !temp.exists() )
+		{
+			throw new FileNotFoundException( root );
+		}
+		_root = root;
+	}
+	
+	public String getRoot()
+	{
+		return _root;
+	}
+	
 	public void write( HTMLBuilder builder )
 		throws IOException
 	{
 		Bill page = getPage();
 		String biennium = page.getBienniumParam();
 		String billNumber = page.getBillNumberParam();
-//		File source = new File( "./../testify.old/data/" + biennium + "/" + billNumber +".txt" );
-		File source = new File( "/Users/jasonosgood/Desktop/testify-data/text" + "/" + billNumber +".txt" );
+		String filename = getRoot() + billNumber +".txt";
+		File source = new File( filename );
 		if( source.exists() )
 		{
 			System.out.println( "found it!" );
 			// TODO: verify file exists.
 			
 			MarkdownRender render = new MarkdownRender();
-//		HTMLBuilder builder = new HTMLBuilder( System.out );
 			render.renderBill( source, builder );
 		}
 		// TODO: throw exception when broken
